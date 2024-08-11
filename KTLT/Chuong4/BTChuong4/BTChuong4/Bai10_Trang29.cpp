16c9cb4c5acc45140419d38f4f427d637fd67a3b
hoangkhoi2003 <hoangkhoitl2003@gmail.com>
2024-08-11 12:53:36 +0700

    BuiHoangKhoi: finish Chuong4 Bai 10 Trang 29

---
+++ KTLT/Chuong4/BTChuong4/BTChuong4/Bai10_Trang29.cpp
@@ -0,0 +1,53 @@
+﻿#include <stdio.h>
+#include <stdlib.h>
+
+int binary_search_recursive(int a[], int left, int right, int x) {
+    if (left <= right) {
+        int mid = left + (right - left) / 2;
+
+        if (a[mid] == x)
+            return mid;
+
+        if (a[mid] > x)
+            return binary_search_recursive(a, left, mid - 1, x);
+
+        return binary_search_recursive(a, mid + 1, right, x);
+    }
+
+    return -1;
+}
+
+int main() {
+    int n, x, result;
+    int* a;
+
+    printf("Nhap so luong phan tu cua mang: ");
+    scanf("%d", &n);
+
+ 
+    a = (int*)malloc(n * sizeof(int));
+    if (a == NULL) {
+        printf("Loi cap phat bo nho!");
+        return 1;
+    }
+
+    printf("Nhap %d phan tu cua mang (theo thu tu tang dan):\n", n);
+    for (int i = 0; i < n; i++) {
+        scanf("%d", &a[i]);
+    }
+
+    printf("Nhap so can tim: ");
+    scanf("%d", &x);
+
+    result = binary_search_recursive(a, 0, n - 1, x);
+
+    if (result == -1)
+        printf("Khong tim thay %d trong mang.\n", x);
+    else
+        printf("%d duoc tim thay tai vi tri %d trong mang.\n", x, result);
+
+  
+    free(a);
+
+    return 0;
+}
